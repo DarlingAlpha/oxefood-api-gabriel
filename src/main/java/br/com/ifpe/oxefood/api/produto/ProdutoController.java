ed37ad28d302b6a86fe9a2b983dafbc67273f746
aluno <aluno@ifpe>
2023-09-22 16:24:20 -0300

    Aula c12 e c13

--- src/main/java/br/com/ifpe/oxefood/api/produto/ProdutoController.java
+++ src/main/java/br/com/ifpe/oxefood/api/produto/ProdutoController.java
@@ -1,9 +1,13 @@
 package br.com.ifpe.oxefood.api.produto;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +18,7 @@ import br.com.ifpe.oxefood.modelo.produto.ProdutoService;
 
 
 @RestController
-@RequestMapping("/api/cliente")
+@RequestMapping("/api/produto")
 @CrossOrigin
 public class ProdutoController {
 
@@ -27,6 +31,17 @@ public class ProdutoController {
        Produto produto = produtoService.save(request.build());
        return new ResponseEntity<Produto>(produto, HttpStatus.CREATED);
    }
+   @GetMapping
+    public List<Produto> findAll() {
+  
+        return produtoService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Produto findById(@PathVariable Long id) {
+
+        return produtoService.findById(id);
+    }
 }
 
 
--- src/main/java/br/com/ifpe/oxefood/modelo/entregador/EntregadorService.java
+++ src/main/java/br/com/ifpe/oxefood/modelo/entregador/EntregadorService.java
@@ -1,6 +1,7 @@
 package br.com.ifpe.oxefood.modelo.entregador;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import javax.transaction.Transactional;
 
@@ -20,5 +21,13 @@ public class EntregadorService {
        entregador.setDataCriacao(LocalDate.now());
        return repository.save(entregador);
    }
+ public List<Entregador> findAll() {
+  
+    return repository.findAll();
+}
 
+public Entregador findById(Long id) {
+
+    return repository.findById(id).get();
+}
 }
