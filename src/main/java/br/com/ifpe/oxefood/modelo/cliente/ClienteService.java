5983240a8c301b4ee510ab0bde2e5d048e6c5f51
aluno <aluno@ifpe>
2023-09-29 15:01:35 -0300

    aula c13 e c14 cliente completo

--- src/main/java/br/com/ifpe/oxefood/modelo/cliente/ClienteService.java
+++ src/main/java/br/com/ifpe/oxefood/modelo/cliente/ClienteService.java
@@ -11,25 +11,38 @@ import org.springframework.stereotype.Service;
 @Service
 public class ClienteService {
     @Autowired
-   private ClienteRepository repository;
+    private ClienteRepository repository;
+   
+    @Transactional
+    public Cliente save(Cliente cliente) {
 
-   @Transactional
-   public Cliente save(Cliente cliente) {
+        cliente.setHabilitado(Boolean.TRUE);
+        cliente.setVersao(1L);
+        cliente.setDataCriacao(LocalDate.now());
+        return repository.save(cliente);
+    }
 
-       cliente.setHabilitado(Boolean.TRUE);
-       cliente.setVersao(1L);
-       cliente.setDataCriacao(LocalDate.now());
-       return repository.save(cliente);
-   }
-   public List<Cliente> findAll() {
-  
-    return repository.findAll();
-}
+    public List<Cliente> findAll() {
 
-public Cliente findById(Long id) {
+        return repository.findAll();
+    }
 
-    return repository.findById(id).get();
-}
+    public Cliente findById(Long id) {
 
+        return repository.findById(id).get();
+    }
+     /* Transactional para alteração do banco*/
+    @Transactional
+    public void update(Long id, Cliente clienteAlterado) {
 
+        Cliente cliente = repository.findById(id).get();
+        cliente.setNome(clienteAlterado.getNome());
+        cliente.setDataNascimento(clienteAlterado.getDataNascimento());
+        cliente.setCpf(clienteAlterado.getCpf());
+        cliente.setFoneCelular(clienteAlterado.getFoneCelular());
+        cliente.setFoneFixo(clienteAlterado.getFoneFixo());
+        /* altera a verção começa de um e vai em diante */
+        cliente.setVersao(cliente.getVersao() + 1);
+        repository.save(cliente);
+    }
 }
