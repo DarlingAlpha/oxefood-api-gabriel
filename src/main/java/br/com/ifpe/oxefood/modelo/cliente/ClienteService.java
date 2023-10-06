7e73ad8334d821bfec2b1b88176637499f6c6dae
aluno <aluno@ifpe>
2023-10-06 15:07:45 -0300

    Aula C16 e C17 completas.

--- src/main/java/br/com/ifpe/oxefood/modelo/cliente/ClienteService.java
+++ src/main/java/br/com/ifpe/oxefood/modelo/cliente/ClienteService.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ClienteService {
+
     @Autowired
     private ClienteRepository repository;
    
@@ -31,6 +32,7 @@ public class ClienteService {
 
         return repository.findById(id).get();
     }
+
      /* Transactional para alteração do banco*/
     @Transactional
     public void update(Long id, Cliente clienteAlterado) {
@@ -45,4 +47,15 @@ public class ClienteService {
         cliente.setVersao(cliente.getVersao() + 1);
         repository.save(cliente);
     }
+
+    @Transactional
+   public void delete(Long id) {
+
+       Cliente cliente = repository.findById(id).get();
+       cliente.setHabilitado(Boolean.FALSE);
+       cliente.setVersao(cliente.getVersao() + 1);
+
+       repository.save(cliente);
+    }
+    
 }
